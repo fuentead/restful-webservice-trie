7bb8159950e2db0aaabb098b2a6082164ff921ed
Adriana Fuentes <afuentes321@gmail.com>
2019-02-07 09:59:14 -0800

    Add files via upload

---
+++ WordDaoService.java
@@ -0,0 +1,43 @@
+package com.rest.webservice.trie.restfulwebservicetrie.word;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class WordDaoService {
+   
+   private Trie trie;
+      
+   public String startTrie() {
+      trie = new Trie();
+      return "Trie Started";
+   }
+   
+   public int loadWordsTrie() {
+      return trie.loadTrie();
+      //return 1;
+   }
+   
+   public List<String> printAllTrieWords() {
+      return trie.seeTrie();
+   }
+   
+   public boolean findTrieWord(String word) {
+      return trie.find(word);
+   }
+   
+   public List<String> suggestWords(String wordprefix) {
+      return trie.suggest(wordprefix);
+   }
+   
+   public Word insertTrieWord(Word word) {
+      if(word == null)
+         System.out.println("Word is null");
+      
+      System.out.println("Word is: " + word);
+      trie.insert(word.getWord());
+      return word;
+   } 
+}
